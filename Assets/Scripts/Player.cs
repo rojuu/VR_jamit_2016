c91929832ed34394abc9fb37218a680737496155
Roni-OttoJup <Roni-OttoJuppi@kamk.fi>
2016-11-05 19:07:43 +0200

    no audio

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,13 +3,42 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+    bool playerWon = false;
+    bool monsterWon = false;
+    
+    // Update is called once per frame
+	void Update ()
+    {
+	    if(GlobalVars.Instance.ChildCount >= GlobalVars.Instance.ChildMax && !monsterWon)
+        {
+            playerWon = true;
+            print("Player won");
+        }
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+    void OnCollisionEnter(Collision col)
+    {
+        if(col.gameObject.tag == "Player2")
+        {
+            if (--GlobalVars.Instance.PlayerHP <= 0 && !playerWon)
+            {
+                monsterWon = true;
+                print("Monster won the game");
+            }
+
+            StartCoroutine(Invicibility());
+        }
+    }
+
+    IEnumerator Invicibility()
+    {
+        SetInvicibility(true);
+        yield return new WaitForSeconds(3);
+        SetInvicibility(false);
+    }
+
+    void SetInvicibility(bool isInvicible)
+    {
+
+    }
 }
